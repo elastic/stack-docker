6bd7498207def65b96593324d5f49deb85381dad
Toby McLaughlin <toby@jarpy.net>
2017-11-30 11:00:45 +1100

    Repro for issue #15

--- scripts/setup-beat.sh
+++ scripts/setup-beat.sh
@@ -3,14 +3,10 @@
 set -euo pipefail
 
 beat=$1
-es_url=http://elastic:${ELASTIC_PASSWORD}@elasticsearch:9200
 
-# Load the sample dashboards for the Beat.
-# REF: https://www.elastic.co/guide/en/beats/metricbeat/master/metricbeat-sample-dashboards.html
-until ${beat} setup \
-        -E setup.kibana.host=kibana \
-        -E setup.kibana.username=elastic \
-        -E setup.kibana.password=${ELASTIC_PASSWORD}
+sleep ${SLEEP_TIME}
+
+until ${beat} setup -E setup.kibana.host=kibana
 do
     sleep 2
     echo Retrying...
