94d816d6db118cc4b8f04b94e1c2d949de12d9df
Toby McLaughlin <toby@jarpy.net>
2018-06-13 15:09:06 -0700

    Remove more Security config for 6.3

--- scripts/setup-beat.sh
+++ scripts/setup-beat.sh
@@ -4,15 +4,11 @@ set -euo pipefail
 
 beat=$1
 
-until curl -s http://kibana:5601; do
+until curl -s http://kibana:5601 > /dev/null; do
     sleep 2
 done
 sleep 5
 
 # Load the sample dashboards for the Beat.
 # REF: https://www.elastic.co/guide/en/beats/metricbeat/master/metricbeat-sample-dashboards.html
-${beat} setup \
-        -E setup.kibana.host=kibana \
-        -E setup.kibana.username=elastic \
-        -E setup.kibana.password=${ELASTIC_PASSWORD} \
-        -E output.elasticsearch.password=${ELASTIC_PASSWORD}
+${beat} setup -E setup.kibana.host=kibana
